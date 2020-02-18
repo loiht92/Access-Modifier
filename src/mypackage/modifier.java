cdea7f5dc325f7795339ba8cd2069aa284f9b668
loiminh <loiho11082015@gmail.com>
2020-02-18 16:28:21 +0700

    add file

---
+++ src/mypackage/modifier.java
@@ -0,0 +1,19 @@
+package mypackage;
+
+public class modifier {
+      public void msg()
+      {
+        System.out.println("Hello");
+    }
+}
+// con proteted thi chi truy cap dk tu ben ngoai thong qua lop ke thua//
+
+//public class modifier {
+//    protected void msg()
+//    {
+//        System.out.println("Hello");
+//    }
+//}
+
+
+ // neu la public void thi co the truy cap no o moi noi//
---
+++ src/mypackage1/TestModifier.java
@@ -0,0 +1,19 @@
+package mypackage1;
+import mypackage.*; // cach su dung import tu package//
+
+public class TestModifier  {
+    public static void main(String[] args) {
+        modifier obj = new modifier();
+        obj.msg();
+    }
+}
+
+
+//public class TestModifier extends modifier {
+//    public static void main(String[] args) {
+//        TestModifier obj = new TestModifier();
+//        obj.msg();
+//    }
+//}
+
+
